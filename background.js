b70a0de3474db54f01a721e0f2590159ec561a81
Md. Khaliduzzaman Khan Samrat <67005838+isaamrat@users.noreply.github.com>
2026-01-19 13:50:20 +0600

    V1.3 with "I" Grade

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.action.onClicked.addListener((tab) => {
+    chrome.sidePanel.setOptions({
+        tabId: tab.id,
+        path: 'sidepanel.html',
+        enabled: true
+    });
+    chrome.sidePanel.open({ tabId: tab.id });
+});
